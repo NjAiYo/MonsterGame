a3bee12b07c6b7680a8b28f224646d8f63484b71
Running John <running727@gmail.com>
2015-04-30 14:21:48 +0800

    a

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -12,7 +12,44 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/MenuScene.cpp \
+                   ../../Classes/BGTTile.cpp \
+                   ../../Classes/BGTMap.cpp \
+                   ../../Classes/WaveManager.cpp \
+                   ../../Classes/Wave.cpp \
+                   ../../Classes/Troop.cpp \
+                   ../../Classes/Util.cpp \
+                   ../../Classes/Unit.cpp \
+                   ../../Classes/Troop.cpp \
+                   ../../Classes/Player.cpp \
+                   ../../Classes/CCShake.cpp \
+                   ../../Classes/UserData.cpp \
+                   ../../Classes/GameManager.cpp \
+                   ../../Classes/UILayer.cpp \
+                   ../../Classes/BGTWorld.cpp \
+                   ../../Classes/GameEntity.cpp \
+                   ../../Classes/BGTWall.cpp \
+                   ../../Classes/Character.cpp \
+                   ../../Classes/GameEntityManager.cpp \
+                   ../../Classes/MessageDispatcher.cpp \
+                   ../../Classes/Enemy.cpp \
+                   ../../Classes/MoveState.cpp \
+                   ../../Classes/StandState.cpp \
+                   ../../Classes/AttackState.cpp \
+                   ../../Classes/FallDownState.cpp \
+                   ../../Classes/DizzyState.cpp \
+                   ../../Classes/InjureState.cpp \
+                   ../../Classes/StandUpState.cpp \
+                   ../../Classes/StiffState.cpp \
+                   ../../Classes/RollBackState.cpp \
+                   ../../Classes/DieState.cpp \
+                   ../../Classes/LieDownState.cpp \
+                   ../../Classes/WeaponState.cpp \
+                   ../../Classes/FlowState.cpp \
+                   ../../Classes/Pistol.cpp \
+                   ../../Classes/Knife.cpp \
+                   ../../Classes/Weapon.cpp \
+                   ../../Classes/LoadScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
